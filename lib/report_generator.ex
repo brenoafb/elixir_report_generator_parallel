42ae649b3a4dd3d4c808dd465bd756180823e2be
brenoafb <breno.afb@gmail.com>
2021-03-10 10:28:39 -0300

    Initial commit

---
+++ lib/report_generator.ex
@@ -0,0 +1,18 @@
+defmodule ReportGenerator do
+  @moduledoc """
+  Documentation for `ReportGenerator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReportGenerator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
